7fa007e7a3de79aebd34a3a4733dd7dc81cf2e76
unknown <lidanyang1111@gmail.com>
2022-03-06 10:34:44 -0800

    version on 03/06/2022

---
+++ review/src/components/PokeList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./../styles.scss";
+import Pokemon from "./Pokemon";
+
+function PokeList(props) {
+    const { pokemen, handlePoke } = props;
+
+    return (
+        <div id="pokeList">
+            {pokemen.map((pokemon) => (
+                <Pokemon pokemon={pokemon} handlePoke={handlePoke} />
+            ))}
+        </div>
+    )
+}
+
+export default PokeList;
---
+++ review/src/components/Pokemon.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles.scss";
+
+const Pokemon = ({ pokemon, handlePoke }) => {
+    return (
+        <div
+            onClick={() => handlePoke(pokemon.id)}
+            key={pokemon.id}
+            className="pokemon"
+        >
+            <img src={pokemon.img} alt={pokemon.name} />
+            <div>
+                <h3>{pokemon.name}</h3>
+                {pokemon.next_evolution &&
+                    pokemon.next_evolution.map((e) => <p key={e.num}>{e.name}</p>)}
+            </div>
+        </div>
+    )
+}
+
+export default Pokemon;
